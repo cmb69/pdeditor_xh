21de642664f00c27135021c7fa6e06adc69feff1
cmb69 <cmbecker69@gmx.de>
2013-12-21 00:33:46 +0000

    refactored out Pdeditor_Views class

--- admin.php
+++ admin.php
@@ -32,33 +32,22 @@ define('PDEDITOR_VERSION', '@PDEDITOR_VERSION@');
  * Returns the plugin about information view.
  *
  * @return string (X)HTML.
+ *
+ * @global Pdeditor_Views The pdeditor views.
  */
 function Pdeditor_version()
 {
+    global $_Pdeditor_views;
+
     $version = PDEDITOR_VERSION;
-    return <<<EOT
+    $o = <<<EOT
 <h1><a href="http://3-magi.net/?CMSimple_XH/Pdeditor_XH">Pdeditor_XH</a></h1>
 <p>Version: $version</p>
 <p>Copyright &copy; 2012-2013 <a href="http://3-magi.net">Christoph M. Becker</a></p>
-<p style="text-align: justify">
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-</p>
-<p style="text-align: justify">
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-</p>
-<p style="text-align: justify">
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <a href="http://www.gnu.org/licenses/">
-    http://www.gnu.org/licenses/</a>.
-</p>
 
 EOT;
+    $o .= $_Pdeditor_views->license();
+    return $o;
 }
 
 /**
@@ -324,6 +313,8 @@ function Pdeditor_adminMain()
         . '</div>' . PHP_EOL;
     return $o;
 }
+
+$_Pdeditor_views = new Pdeditor_Views();
 
 /*
  * Handle the plugin administration.
