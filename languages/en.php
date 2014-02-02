af66b89d4b7c3bd6af9aefad7f12bc1caddadc67
cmb69 <cmbecker69@gmx.de>
2014-02-02 23:11:44 +0000

    added German and revised English localization

--- languages/en.php
+++ languages/en.php
@@ -2,21 +2,21 @@
 
 $plugin_tx['pdeditor']['menu_main']="Pagedata";
 
-$plugin_tx['pdeditor']['info_about']="About";
+$plugin_tx['pdeditor']['info_about']="About Pdeditor_XH";
 $plugin_tx['pdeditor']['info_heading']="Info";
 
 $plugin_tx['pdeditor']['label_delete']="Delete this attribute";
 $plugin_tx['pdeditor']['label_attribute']="Attribute: %s";
 $plugin_tx['pdeditor']['label_attributes']="Attributes";
-$plugin_tx['pdeditor']['warning_delete']="Deleting an attribute might cause malfunctions of the system!";
-$plugin_tx['pdeditor']['warning_save']="Modifying the pagedata might cause malfunctions of the system!";
+$plugin_tx['pdeditor']['warning_delete']="Deleting an attribute may cause malfunctions of the system!";
+$plugin_tx['pdeditor']['warning_save']="Modifying the pagedata may cause malfunctions of the system!";
 $plugin_tx['pdeditor']['message_headings']="The headings of the content and the pagedata don't match!";
 
 $plugin_tx['pdeditor']['syscheck_title']="System check";
 $plugin_tx['pdeditor']['syscheck_phpversion']="PHP version &ge; %s";
-$plugin_tx['pdeditor']['syscheck_extension']="Extension '%s' loaded";
-$plugin_tx['pdeditor']['syscheck_encoding']="Encoding 'UTF-8' configured";
-$plugin_tx['pdeditor']['syscheck_magic_quotes']="Magic quotes runtime off";
-$plugin_tx['pdeditor']['syscheck_writable']="Folder '%s' writable";
+$plugin_tx['pdeditor']['syscheck_extension']="Extension \"%s\" loaded";
+$plugin_tx['pdeditor']['syscheck_encoding']="UTF-8 encoding configured";
+$plugin_tx['pdeditor']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['pdeditor']['syscheck_writable']="Folder \"%s\" writable";
 
 ?>
