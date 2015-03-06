f5d945582b99bb288c9a4396c5ec9cf4bf40adfe
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-06 22:06:44 +0100

    refactored pdeditor.js to work also with older browsers (e.g. IE8)

--- pdeditor.js
+++ pdeditor.js
@@ -7,21 +7,82 @@
 
 /*jslint browser: true, maxlen: 80 */
 
-if (typeof window.addEventListener !== "undefined") {
-    window.addEventListener("load", function () {
-        "use strict";
+(function () {
+    "use strict";
 
+    /**
+     * Gets elements by class name.
+     *
+     * @param {Element} element
+     * @param {string}  className
+     *
+     * @returns {Array}
+     */
+    function getByClassName(element, className) {
+        var regex, children, i, len, child, result;
+
+        result = [];
+        regex = new RegExp("(^|\\s)" + className + "(\\s|$)");
+        children = element.getElementsByTagName("*");
+        for (i = 0, len = children.length; i < len; i += 1) {
+            child = children[i];
+            if (regex.test(child.className)) {
+                result.push(child);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Calls a callback for each element.
+     *
+     * @param {Array}    elements
+     * @param {Function} func
+     *
+     * @returns {undefined}
+     */
+    function forEach(elements, func) {
+        var i, len;
+
+        for (i = 0, len = elements.length; i < len; i += 1) {
+            func(elements[i], i);
+        }
+    }
+
+    /**
+     * Registers an event listener.
+     *
+     * @param {EventTarget}   target
+     * @param {string}        type
+     * @param {EventListener} listener
+     *
+     * @returns {undefined}
+     */
+    function on(target, type, listener) {
+        if (typeof target.addEventListener !== "undefined") {
+            target.addEventListener(type, listener, false);
+        } else if (typeof target.attachEvent !== "undefined") {
+            target.attachEvent("on" + type, listener);
+        }
+    }
+
+    /**
+     * Initializes the page data editor.
+     *
+     * @returns {undefined}
+     */
+    function init() {
         var attribute, attributes, select, headings;
 
         /*
-         * Returns the current page data attribute from the query string.
+         * Returns the requested page data attribute from the query string.
          *
          * @returns {string}
          */
-        function getAttribute() {
+        function getRequestedAttribute() {
             var parts, i, pair;
 
-            parts = window.location.search.substr(1).split("&");
+            parts = location.search.substr(1).split("&");
             for (i = 0; i < parts.length; i += 1) {
                 pair = parts[i].split("=");
                 if (pair[0] === "pdeditor_attr") {
@@ -31,35 +92,46 @@ if (typeof window.addEventListener !== "undefined") {
             return "url";
         }
 
-        attribute = getAttribute();
-        select = document.createElement("select");
-        attributes = document.querySelectorAll("#pdeditor_attr > li");
-        attributes = Array.prototype.slice.call(attributes);
-        attributes = attributes.forEach(function (el) {
-            var option, anchor;
+        /**
+         * Adds an option to the select element.
+         *
+         * @param {HTMLLiElement} item
+         *
+         * @returns {undefined}
+         */
+        function addOption(item) {
+            var option, anchor, text;
 
-            anchor = el.getElementsByTagName("a")[0];
+            anchor = item.getElementsByTagName("a")[0];
             option = document.createElement("option");
-            option.text = anchor.textContent;
-            if (option.text === attribute) {
+            text = anchor.textContent || anchor.innerText;
+            if (text === attribute) {
                 option.selected = true;
             }
+            option.appendChild(document.createTextNode(text));
             option.value = anchor.href;
             select.appendChild(option);
-        });
-        select.addEventListener("change", function (event) {
-            window.location = event.target.value;
+        }
+
+        attribute = getRequestedAttribute();
+        select = document.createElement("select");
+        attributes = document.getElementById("pdeditor_attr").
+                getElementsByTagName("li");
+        forEach(attributes, addOption);
+        on(select, "change", function (event) {
+            location.href = (event.target || event.srcElement).value;
         });
         document.getElementById("pdeditor_attr").parentNode.replaceChild(
             select,
             document.getElementById("pdeditor_attr")
         );
-        headings = document.querySelectorAll(".pdeditor_heading");
-        headings = Array.prototype.slice.call(headings);
-        headings.forEach(function (el) {
+        headings = getByClassName(document, "pdeditor_heading");
+        forEach(headings, function (el) {
             el.parentNode.removeChild(el);
         });
         document.getElementById("pdeditor_delete").style.display =
                 "inline-block";
-    });
-}
+    }
+
+    on(window, "load", init);
+}());
